d384d6f5a8f05fd595815697ec0bd412876c0ba8
Max Holman <max@holmn.com>
2023-04-28 16:03:56 +0800

    feat: added client file

--- src/build.ts
+++ src/build.ts
@@ -1,8 +1,6 @@
 import { writeFile } from 'node:fs/promises';
-import { join } from 'node:path';
 import type { OpenAPIV3 } from 'openapi-types';
 import { format } from 'prettier';
-import { IndentationText, Project, QuoteKind, ScriptTarget } from 'ts-morph';
 import { processOpenApiDocument } from '../lib/process-document.js';
 
 export async function build(
@@ -14,51 +12,19 @@ export async function build(
     assert: { type: 'json' },
   })) as { default: OpenAPIV3.Document };
 
-  const project = new Project({
-    compilerOptions: {
-      target: ScriptTarget.ES2022,
-      declaration: true,
-    },
-    manipulationSettings: {
-      quoteKind: QuoteKind.Single,
-      indentationText: IndentationText.TwoSpaces,
-      useTrailingCommas: true,
-    },
-  });
-
-  const banner = `
-  /**
-   * This file is auto generated.
-   *
-   * WARN: Do not edit directly.
-   *
-   * Generated on ${new Date().toISOString()}
-   *
-  */`.trim();
-
-  const entryFile = project.createSourceFile(
-    join(outputDir, 'methods.ts'),
-    banner,
-    {
-      overwrite: true,
-    },
+  const { entryFile, typesFile, clientFile } = await processOpenApiDocument(
+    outputDir,
+    apischema.default,
+    tags,
   );
-
-  const typesFile = project.createSourceFile(
-    join(outputDir, 'models.ts'),
-    banner,
-    {
-      overwrite: true,
-    },
-  );
-
-  await processOpenApiDocument(entryFile, typesFile, apischema.default, tags);
 
   entryFile.formatText();
   typesFile.formatText();
+  clientFile.formatText();
 
   await entryFile.save();
   await typesFile.save();
+  await clientFile.save();
 
   await Promise.all(
     [entryFile, entryFile].map((file) =>
