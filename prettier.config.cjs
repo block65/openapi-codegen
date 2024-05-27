63e15fad6cd148b355a5cfee362e77095f0210a2
Max Holman <max@holmn.com>
2024-05-27 18:00:28 +0800

    chore: rename

--- prettier.config.cjs
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-module.exports = require('@block65/eslint-config/prettier.config.cjs');
---
+++ prettier.config.mjs
@@ -0,0 +1,3 @@
+import config from '@block65/eslint-config/prettier.config.cjs';
+
+export default config;
